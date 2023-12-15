1f400075591f2d6de1afe43a48c09f941f494728
Ian Septiana <insaidseptiana@gmail.com>
2023-12-15 10:27:50 +0700

    link barcode rak & barang implemented

--- action/barcodebrg/fetchBarcode.php
+++ action/barcodebrg/fetchBarcode.php
@@ -15,7 +15,7 @@ if ($stmt = $koneksi->prepare($sql)) {
 	$output = array('data' => array());
 
 	while ($row = $result->fetch_array()) {
-		$button = generateButton($row['id_brg']);
+		$button = generateButton($row['id_brg'], $row['barcode_brg']);
 
 		$output['data'][] = array(
 			$row['barcode_brg'],
@@ -36,13 +36,14 @@ $koneksi->close();
 
 echo json_encode($output);
 
-function generateButton($id_brg)
+function generateButton($id_brg, $barcode)
 {
 	return '<div class="btn-group">
         <button data-toggle="dropdown" class="btn btn-small btn-primary dropdown-toggle">Action <span class="caret"></span></button>
         <ul class="dropdown-menu">
             <li><a href="#editModalBarang" onclick="editBarang(' . $id_brg . ')" data-toggle="modal"><i class="icon-pencil"></i> Edit</a></li>
             <li><a href="#hapusModalBarang" onclick="hapusBarang(' . $id_brg . ')" data-toggle="modal"><i class="icon-trash"></i> Hapus</a></li>
+            <li><a href="http://192.168.1.7/generator-qrcode/index.php?value=' . $barcode . '" target="_blank" rel="noreferrer noopener"><i class="fa fa-qrcode"></i> Barcode</a></li>
         </ul>
     </div>';
 }
--- action/barcoderak/fetchBarcode.php
+++ action/barcoderak/fetchBarcode.php
@@ -15,7 +15,7 @@ if ($stmt = $koneksi->prepare($sql)) {
 	$output = array('data' => array());
 
 	while ($row = $result->fetch_array()) {
-		$button = generateButton($row['id_rak']);
+		$button = generateButton($row['id_rak'], $row['barcode_rak']);
 
 		$output['data'][] = array(
 			$row['barcode_rak'],
@@ -34,13 +34,14 @@ $koneksi->close();
 
 echo json_encode($output);
 
-function generateButton($id_brg)
+function generateButton($id_brg, $barcode)
 {
 	return '<div class="btn-group">
         <button data-toggle="dropdown" class="btn btn-small btn-primary dropdown-toggle">Action <span class="caret"></span></button>
         <ul class="dropdown-menu">
             <li><a href="#editModalBarang" onclick="editBarang(' . $id_brg . ')" data-toggle="modal"><i class="icon-pencil"></i> Edit</a></li>
             <li><a href="#hapusModalBarang" onclick="hapusBarang(' . $id_brg . ')" data-toggle="modal"><i class="icon-trash"></i> Hapus</a></li>
+			<li><a href="http://192.168.1.7/generator-qrcode/index.php?value=' . $barcode . '" target="_blank" rel="noreferrer noopener"><i class="fa fa-qrcode"></i> Barcode</a></li>
         </ul>
     </div>';
 }
