802e2c9c89a421de95f259acf760d672b9207937
Ian Septiana <insaidseptiana@gmail.com>
2023-12-16 14:27:34 +0700

    refactore save pomasuk

--- action/pomasuk/save.php
+++ action/pomasuk/save.php
@@ -6,6 +6,7 @@ require_once '../class/barcodebarang.php';
 require_once '../class/pomasuk.php';
 require_once '../class/masuk.php';
 
+define('STATUS', 'INPG');
 
 $valid['success'] =  array('success' => false, 'messages' => array());
 
@@ -17,17 +18,9 @@ $pomasuk = new PoMasuk($koneksi);
 $masuk = new Masuk($koneksi);
 
 try {
-    $tgl  = trim($koneksi->real_escape_string($_POST["tgl"]));
-    $noPO = trim($koneksi->real_escape_string($_POST["nopo"]));
-    $barang = trim($koneksi->real_escape_string($_POST["item"]));
-    $nopol = trim($koneksi->real_escape_string($_POST["nopol"]));
-    $qty = trim($koneksi->real_escape_string($_POST["qty"]));
-    $ket = trim(
-        $koneksi->real_escape_string(
-            isset($_POST["note"]) && !empty($_POST["note"]) ? $_POST["note"] : ""
-        )
-    );
+    $inputs = getInputs($koneksi);
     $nama = $_SESSION['nama'];
+    extract($inputs);
 
     $checkNoPO = $masuk->getNoPO($noPO, $tgl);
 
@@ -36,69 +29,105 @@ try {
     $idBrg  = $resultBarcodeBrg['id_brg'];
 
     if ($checkNoPO->num_rows == 1) {
-
-        if ($checkBarcodeBrg->num_rows == 1) {
-            $resultMasuk = $checkNoPO->fetch_array();
-            $idMsk  = $resultMasuk['id_msk'];
-            $status = 'INPG';
-
-            $insertPoMasuk = $pomasuk->insertPoMasuk($idMsk, $idBrg, $qty, $nopol, $status, $ket, $nama);
-
-            if ($insertPoMasuk) {
-                $valid['success'] = true;
-                $valid['messages'] = "<strong>Success! </strong>Data Berhasil Disimpan";
-                $sql_success = true;
-            } else {
-                $valid['success'] = false;
-                $valid['messages'] = "<strong>Error! </strong> Data Gagal Disimpan.";
-            }
-        } else {
-            $valid['success'] = false;
-            $valid['messages'] = "<strong>Error! </strong> Barang Tidak Ada Barcode.";
-        }
+        handleExistingNoPO($checkBarcodeBrg, $checkNoPO, $pomasuk, $idBrg, $qty, $nopol, $ket, $nama);
     } else if ($checkNoPO->num_rows == 0) {
-        if ($checkBarcodeBrg->num_rows == 1) {
-            $insertMasuk = $masuk->insertMasuk($noPO, $tgl, $nama);
-
-            if ($insertMasuk['success']) {
-                $lastIdMsk  = $insertMasuk['id'];
-                $status = 'INPG';
-
-                $insertPoMasuk = $pomasuk->insertPoMasuk($lastIdMsk, $idBrg, $qty, $nopol, $status, $ket, $nama);
-
-                if ($insertPoMasuk) {
-                    $valid['success'] = true;
-                    $valid['messages'] = "<strong>Success! </strong>Data Berhasil Disimpan";
-                    $sql_success = true;
-                } else {
-                    $valid['success'] = false;
-                    $valid['messages'] = "<strong>Error! </strong> Data Gagal Disimpan.";
-                }
-            } else {
-                $valid['success'] = false;
-                $valid['messages'] = "<strong>Error! </strong> Data Gagal Disimpan.";
-            }
-        } else {
-            $valid['success'] = false;
-            $valid['messages'] = "<strong>Error! </strong> Barang Tidak Ada Barcode.";
-        }
+        handleNewNoPO($checkBarcodeBrg, $masuk, $pomasuk, $noPO, $tgl, $nama, $idBrg, $qty, $nopol, $ket);
     } else {
         $valid['success'] = false;
         $valid['messages'] = "<strong>Error! </strong> Surat Jalan Duplikat.";
     }
 } catch (\Throwable $th) {
+    error_log($th->getMessage()); // Log the error
     $valid['success'] = false;
     $valid['messages'] = "<strong>Error! </strong> Terjadi Kesalahan Hubungi Staf IT.";
 } finally {
     if ($sql_success) {
-
-        $koneksi->commit(); //simpan semua data simpan
-
+        $koneksi->commit();
     } else {
-
-        $koneksi->rollback(); //batal semua data simpan
-
+        $koneksi->rollback();
     }
     $koneksi->close();
     echo json_encode($valid);
+}
+
+function getInputs($koneksi)
+{
+    $inputs = [
+        "tgl" => trim($koneksi->real_escape_string($_POST["tgl"])),
+        "noPO" => trim($koneksi->real_escape_string($_POST["nopo"])),
+        "barang" => trim($koneksi->real_escape_string($_POST["item"])),
+        "nopol" => trim($koneksi->real_escape_string($_POST["nopol"])),
+        "qty" => trim($koneksi->real_escape_string($_POST["qty"])),
+        "ket" => trim(
+            $koneksi->real_escape_string(
+                isset($_POST["note"]) && !empty($_POST["note"]) ? $_POST["note"] : ""
+            )
+        )
+    ];
+
+    return $inputs;
+}
+
+function handleExistingNoPO($checkBarcodeBrg, $checkNoPO, $pomasuk, $idBrg, $qty, $nopol, $ket, $nama)
+{
+    global $valid, $sql_success;
+    if ($checkBarcodeBrg->num_rows != 1) {
+        $valid['success'] = false;
+        $valid['messages'] = "<strong>Error! </strong> Barang Tidak Ada Barcode.";
+        return $valid;
+    }
+
+    $resultMasuk = $checkNoPO->fetch_array();
+    $idMsk  = $resultMasuk['id_msk'];
+    $status = STATUS;
+
+    $insertPoMasuk = $pomasuk->insertPoMasuk($idMsk, $idBrg, $qty, $nopol, $status, $ket, $nama);
+
+    if (!$insertPoMasuk['success']) {
+        $valid['success'] = false;
+        $valid['messages'] = "<strong>Error! </strong> Data Gagal Disimpan di PoMasuk.";
+        return $valid;
+    }
+
+    $valid['success'] = true;
+    $valid['messages'] = "<strong>Success! </strong>Data Berhasil Disimpan";
+    $sql_success = true;
+
+    return $valid;
+}
+
+function handleNewNoPO($checkBarcodeBrg, $masuk, $pomasuk, $noPO, $tgl, $nama, $idBrg, $qty, $nopol, $ket)
+{
+    global $valid, $sql_success;
+
+    if ($checkBarcodeBrg->num_rows != 1) {
+        $valid['success'] = false;
+        $valid['messages'] = "<strong>Error! </strong> Barang Tidak Ada Barcode.";
+        return $valid;
+    }
+
+    $insertMasuk = $masuk->insertMasuk($noPO, $tgl, $nama);
+
+    if (!$insertMasuk['success']) {
+        $valid['success'] = false;
+        $valid['messages'] = "<strong>Error! </strong> Data Gagal Disimpan di Masuk.";
+        return $valid;
+    }
+
+    $lastIdMsk  = $insertMasuk['id'];
+    $status = STATUS;
+
+    $insertPoMasuk = $pomasuk->insertPoMasuk($lastIdMsk, $idBrg, $qty, $nopol, $status, $ket, $nama);
+
+    if (!$insertPoMasuk) {
+        $valid['success'] = false;
+        $valid['messages'] = "<strong>Error! </strong> Data Gagal Disimpan di PoMasuk.";
+        return $valid;
+    }
+
+    $valid['success'] = true;
+    $valid['messages'] = "<strong>Success! </strong>Data Berhasil Disimpan";
+    $sql_success = true;
+
+    return $valid;
 }
