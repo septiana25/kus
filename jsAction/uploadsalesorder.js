ce25f8935113ef3edccc58b49cc7356fe49906d7
Ian Septiana <insaidseptiana@gmail.com>
2024-07-17 14:24:20 +0700

    add tampilan upload SO

---
+++ jsAction/uploadsalesorder.js
@@ -0,0 +1,184 @@
+$(document).ready(function() {
+	let tabelKoresiMinus;
+	$('#activeUpload').addClass('active');
+	$('#activeUploadKoreksiMinus').addClass('active');
+
+	tabelKoresiMinus = $('#tabelKoreksiMinus').DataTable({
+		'ajax' : 'action/upload/fetchkoreksisaldo.php?type=3',
+		'order':[],
+	});
+	
+	$('#checkingData').click(function() {
+		$.ajax({
+			url: 'action/upload/checkingkoreksisaldo.php',
+			type: 'POST',
+			data: { type: '3' },
+			dataType: 'json',
+			success: handleResponse
+		});
+	});
+
+	$('#processData').click(function() {
+		$.ajax({
+			url: 'action/upload/processkoreksiminus.php',
+			dataType: 'json',
+			success: handleResponse
+		});
+	});
+
+	$('#submitUploadKoreksiMinus').unbind('submit').bind('submit', function() {
+		const file = $('#file-csv').val().trim();
+		const type = $('#type').val().trim();
+
+		validateInput(file, '#file-csv', 'File CSV harus diisi');
+		validateInput(type, '#file-csv', 'Relode Page');
+
+		if (file !== '' && type !== '') {
+			const form = $(this);
+			const formData = new FormData(this);
+
+			$.ajax({
+				url: 'action/upload/uploadkoreksi.php',
+				type: 'POST',
+				data: formData,
+				dataType: 'json',
+				cache: false,
+				contentType: false,
+				processData: false,
+				success: handleResponse
+			});
+		}
+		return false;
+
+	});
+
+	$('#submitEditKoreksiMinus').unbind('submit').bind('submit', function() {
+		const id = $('#id').val().trim();
+		const kdbrg = $('#kdbrg').val().trim();
+		const rak = $('#rak').val().trim();
+		const tahunprod = $('#tahunprod').val().trim();
+
+		validateInput(kdbrg, '#kdbrg', 'Kode Barang harus diisi');
+		validateInput(rak, '#rak', 'Rak harus diisi');
+		validateInput(tahunprod, '#tahunprod', 'Tahun harus diisi');
+
+		if (id && kdbrg && rak && tahunprod) {
+			const form = $(this);
+
+			$.ajax({
+				url: form.attr('action'),
+				type: form.attr('method'),
+				data: form.serialize(),
+				dataType: 'json',
+				success: handleResponse
+			});
+		}
+		return false;
+
+	});
+
+	$('#submitDeleteKoreksiMinus').unbind('submit').bind('submit', function() {
+		const id = $('#hapusid').val().trim();
+
+		if (id) {
+			const form = $(this);
+
+			$.ajax({
+				url: form.attr('action'),
+				type: form.attr('method'),
+				data: form.serialize(),
+				dataType: 'json',
+				success: handleResponse
+			});
+		}
+		return false;
+	});
+
+
+	function validateInput(value, selector, errorMessage) {
+		if (value === "") {
+			$(selector).after(`<span class="help-inline">${errorMessage}</span>`);
+			$(selector).closest('.control-group').addClass('error');
+		} else {
+			$(selector).closest('.control-group').addClass('success');
+			$(".help-inline").remove();
+		}
+	}
+	function handleResponse(response) {
+	
+		tabelKoresiMinus.ajax.reload();
+		if (response.success === true) {
+			$('.modal').modal('hide');
+			displayMessagePopup(response.messages, 'success');
+		} else if (response.success === false) {
+			displayMessagePopup(response.messages, 'error');
+		}
+	}
+	
+	function displayMessagePopup(messages, type) {
+		const isSuccessful = type === 'success';
+		const data = {
+			title: isSuccessful ? 'Success!' : 'Error!',
+			image: isSuccessful ? 'img/success-mini.png' : 'img/error-mini.png',
+			class_name: isSuccessful ? 'my-sticky-class' : 'gritter-light'
+		};
+	
+		const unique_id = $.gritter.add({
+			title: data.title,
+			text: messages,
+			image: data.image,
+			class_name: data.class_name,
+			time: ''
+		});
+	
+		setTimeout(function() {
+			$.gritter.remove(unique_id, {
+				fade: true,
+				speed: 'slow'
+			});
+		}, 6000);
+	}
+
+
+//pesan error ajax
+$(document).ajaxError(function(){
+	alert("Terjadi Kesalahan, Lakukan Refresh Halaman. Lihat error_log");
+});
+	
+});
+
+function editKoreksiSaldo(id) {
+	$.ajax({
+		url: 'action/upload/fetchkoreksisaldobyid.php',
+		type: 'POST',
+		data: { id: id },
+		dataType: 'json',
+		success: function(data) {
+			$('#id').val(data.id);
+			$('#kdbrg').val(data.kdbrg);
+			$('#rak').val(data.rak);
+			$('#tahunprod').val(data.tahunprod);
+		}
+	});
+}
+
+function deleteKoreksiSaldo(id) {
+	$.ajax({
+		url: 'action/upload/fetchkoreksisaldobyid.php',
+		type: 'POST',
+		data: { id: id },
+		dataType: 'json',
+		success: function(data) {
+			$('#hapusid').val(data.id);
+			$('#pesanHapus').text('Apakah anda yakin ingin menghapus data ' + data.brg + ' ?');
+		}
+	});
+}
+
+function validAngka(a)
+{
+  if(!/^[0-9.]+$/.test(a.value))
+  {
+  a.value = a.value.substring(0,a.value.length-1000);
+  }
+}
---
+++ uploadsalesorder.php
@@ -0,0 +1,200 @@
+<?php require_once 'function/koneksi.php';
+require_once 'function/session.php';
+
+require_once 'include/header.php';
+require_once 'include/menu.php';
+
+?>
+
+<!-- BEGIN PAGE -->
+<div id="main-content">
+
+    <!-- BEGIN PAGE CONTAINER-->
+    <div class="container-fluid">
+        <!-- BEGIN PAGE HEADER-->
+        <div class="row-fluid">
+            <div class="span12">
+                <h3 class="page-title">
+                    Upload Sales Order
+                </h3>
+                <ul class="breadcrumb">
+                    <li>
+                        <a href="#">Home</a>
+                        <span class="divider">/</span>
+                    </li>
+                    <li>
+                        <a href="#">Upload</a>
+                        <span class="divider">/</span>
+                    </li>
+                    <li class="active">
+                        Upload Seles Order
+                    </li>
+
+                </ul>
+                <!-- END PAGE TITLE & BREADCRUMB-->
+            </div>
+        </div>
+        <!-- END PAGE HEADER-->
+
+        <!-- BEGIN ADVANCED TABLE widget-->
+        <div class="row-fluid">
+            <div class="span12">
+                <!-- BEGIN EXAMPLE TABLE widget-->
+                <div class="widget red">
+                    <div class="widget-title">
+                        <span class="tools">
+                            <a href="javascript:;" class="icon-chevron-down"></a>
+                            <!-- <a href="javascript:;" class="icon-remove"></a> -->
+                        </span>
+                    </div>
+                    <div class="widget-body">
+
+                        <!-- BEGIN FORM LAPORAN MASUK-->
+                        <div class="tabbable custom-tab">
+                            <ul class="nav nav-tabs">
+                                <li class="active"><a href="#tab_1_1" data-toggle="tab">Data</a></li>
+                                <li class=""><a href="#tab_1_2" data-toggle="tab">Form</a></li>
+                            </ul>
+                            <div class="tab-content">
+                                <div class="tab-pane active" id="tab_1_1">
+                                    <p>Data Sales Order</p>
+                                    <div class="form-actions">
+                                        <button class="btn btn-primary" type="button" id="checkingData"><i class="fa fa-check"></i> Check Data</button>
+                                        <button class="btn btn-warning" type="button" id="processData"><i class="fa fa-cogs"></i> Prosess Koreksi</button>
+                                    </div>
+                                    <table class="table table-striped table-bordered" id="tabelSalesOrder">
+                                        <thead>
+                                            <tr>
+                                                <th width="10%">Supir</th>
+                                                <th width="10%">Toko</th>
+                                                <th width="10%">Kode Barang</th>
+                                                <th>Nama Barang</th>
+                                                <th width="10%">Lokasi Rak</th>
+                                                <th width="10%">QTY</th>
+                                                <th width="12%">Status</th>
+                                                <?php
+                                                if ($_SESSION['level'] == "administrator") {
+                                                    echo '<th width="8%" class="hidden-phone">Action</th>';
+                                                }
+                                                ?>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="tab-pane" id="tab_1_2">
+                                    <p>Form Upload Sales Order</p>
+
+                                    <form class="form-horizontal" method="POST" id="submitUploadSalesOrder" enctype="multipart/form-data">
+                                        <div class="control-group">
+                                            <label class="control-label">File CSV </label>
+                                            <div class="controls">
+                                                <input class="span12" type="hidden" id="type" name="type" value="3" readonly>
+                                                <input type="file" class="form-control" id="file-csv" placeholder="File CSV" multiple="true" name="file-csv" class="file-loading" style="width:auto;" accept=".csv" />
+                                            </div>
+                                            <div class="controls">
+                                                <span class="label label-important">NOTE!</span>
+                                                <span>
+                                                    File Harus Berformat CSV
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div class="form-actions">
+                                            <button class="btn btn-success" type="submit" id="uploadFile"><i class="fa fa-upload"></i> Upload</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+
+                            <!-- END BEGIN FORM LAPORAN MASUK-->
+                        </div>
+                    </div>
+                    <!-- END EXAMPLE TABLE widget-->
+                </div>
+            </div>
+
+            <!-- END ADVANCED TABLE widget-->
+        </div>
+
+        <!-- BEGIN MODAL EDIT Sales Order-->
+        <div id="editModalKoreksiSaldo" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h3 id="myModalLabel">Edit Sales Order</h3>
+            </div>
+            <form class="form-horizontal" id="submitEditKoreksiMinus" action="action/upload/updatekoreksisaldo.php" method="POST">
+                <div class="modal-body modal-full">
+                    <div class="control-group">
+                        <div id="infoMinus"></div>
+                        <div id="pesan"></div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label" for="kdbrg">Kode Barang</label>
+                        <div class="controls">
+                            <input class="span12" type="hidden" id="id" name="id" readonly>
+                            <input class="span12" type="text" id="kdbrg" name="kdbrg" autocomplete="off" placeholder="Kode Barang">
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label" for="rak">Rak</label>
+                        <div class="controls">
+                            <input class="span12" type="text" id="rak" name="rak" autocomplete="off" placeholder="Rak">
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label" for="tahunprod">Tahun</label>
+                        <div class="controls">
+                            <input class="span12" type="text" id="tahunprod" name="tahunprod" autocomplete="off" placeholder="Tahun Produksi" minlength="4" maxlength="4" onkeyup="validAngka(this)">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+                    <button class="btn btn-primary" type="submit" id="update">Simpan</button>
+                </div>
+            </form>
+        </div>
+        <!-- END MODAL EDIT Sales Order-->
+
+        <!-- BEGIN MODAL HAPUS Sales Order-->
+        <div id="deleteModalKoreksiSaldo" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h3 id="myModalLabel">Hapus Sales Order</h3>
+            </div>
+            <form class="form-horizontal" id="submitDeleteKoreksiMinus" action="action/upload/deletekoreksisaldo.php" method="POST">
+                <div class="modal-body modal-full">
+                    <p id="pesanHapus" style="color: #dc5d3a"></p>
+                    <input class="span12" type="hidden" id="hapusid" name="hapusid" readonly>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+                    <button class="btn btn-primary" type="submit" id="update">Hapus</button>
+                </div>
+            </form>
+        </div>
+        <!-- END MODAL HAPUS Sales Order-->
+        <!-- BEGIN MODAL DISABLE Sales Order-->
+        <div id="disableaccess" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h3 id="myModalLabel">WARNING</h3>
+            </div>
+            <div class="modal-body modal-full">
+                <p id="pesanHapus" style="color: #dc5d3a">DATA SUDAH DICEK. TIDAK BISA UBAH</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+
+            </div>
+        </div>
+        <!-- END MODAL HAPUS Sales Order-->
+        <!-- END PAGE CONTAINER-->
+    </div>
+    <!-- END PAGE -->
+
+    <?php require_once 'include/footer.php'; ?>
+
+    <script src="jsAction/uploadkoreskiminus.js"></script>
